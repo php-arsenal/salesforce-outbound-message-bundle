57eb5b3885c421fda54c854545b16457188b2d08
Gyvastis <shibalba1@gmail.com>
2021-04-13 11:15:52 +0200

    update config tree

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -9,7 +9,7 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('arsenal_salesforce_outbound_message');
+        $treeBuilder = new TreeBuilder('salesforce_outbound_message');
         $treeBuilder
             ->getRootNode()
             ->children()
